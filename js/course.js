9b656f01b3e5a2b9549ff3f2d80cb944114bfd7d
zhujun24 <zhujun87654321@gmail.com>
2015-08-05 18:20:06 +0800

    dinner

---
+++ js/course.js
@@ -0,0 +1,9 @@
+//$('.nav-link').click(function () {
+//  console.log(1);
+//
+//  if($('.slide').hasClass('slide-transition-backward')){
+//    $('.slide').addClass('slide-transition-forward').removeClass('slide-transition-backward');
+//  }else{
+//    $('.slide').addClass('slide-transition-backward').removeClass('slide-transition-forward');
+//  }
+//});
