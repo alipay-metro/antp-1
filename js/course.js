1ec8be36e280ddbbe961b5a050bf0d5626922ea7
zhujun24 <zhujun87654321@gmail.com>
2015-08-06 17:41:12 +0800

    complete

--- js/course.js
+++ js/course.js
@@ -1,9 +1,35 @@
-//$('.nav-link').click(function () {
-//  console.log(1);
-//
-//  if($('.slide').hasClass('slide-transition-backward')){
-//    $('.slide').addClass('slide-transition-forward').removeClass('slide-transition-backward');
-//  }else{
-//    $('.slide').addClass('slide-transition-backward').removeClass('slide-transition-forward');
-//  }
-//});
+var start = $('input[type=hidden]').val();
+
+$('.slide').css('left', (start - 1) * 144 + 'px');
+
+$('.nav-link').each(function (index) {
+  $(this).hover(function () {
+    var end = index + 1;
+    if (start !== end) {
+      $('.slide').attr('class', 'slide move' + start + end);
+      start = end;
+    }
+  });
+});
+
+var timer1, timer2;
+
+$('.nav-link').eq(2).hover(function () {
+  clearTimeout(timer2);
+  $('.dropdown').removeClass('close').addClass('open');
+}, function () {
+  timer1 = setTimeout(function () {
+    $('.dropdown').removeClass('open').addClass('close');
+  }, 300);
+});
+
+$('.dropdown').mouseleave(function () {
+  timer2 = setTimeout(function () {
+    $('.dropdown').removeClass('open').addClass('close');
+  }, 300);
+});
+
+$('.dropdown').mouseenter(function () {
+  clearTimeout(timer1);
+  $('.dropdown').removeClass('close').addClass('open');
+});
